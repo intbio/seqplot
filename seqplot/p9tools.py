9e7e1f574a15a7c3e09bf915203ecfe9ce4803fd
Alexey Shaytan <alexsha@newton.bioeng.ru>
2020-03-16 12:03:28 +0300

    Update

--- seqplot/p9tools.py
+++ seqplot/p9tools.py
@@ -33,20 +33,22 @@ class geom_seq_x(geom_tile):
                       'na_rm': False}
 
     
-    def __init__(self, seqimg=None, x=None, y=None, aspect_ratio=0.1, mapping=None, data=None, **kwargs):
+    def __init__(self, seqimg=None, x=None, y=None, aspect_ratio=0.1, ymin=None, mapping=None, data=None, **kwargs):
 
         d=mpimg.imread(io.BytesIO(seqimg))
         pict=pd.DataFrame([(i[1],-i[0],to_hex(d[i])) for i in np.ndindex(d.shape[0],d.shape[1])],columns=['x','y','c'])
-        imgdx=d.shape[1]
-        imgdy=d.shape[0]
-        dy=(y.max()-y.min())
+        imgdx=float(d.shape[1])
+        imgdy=float(d.shape[0])
+        dy=(y.max()-np.min(y.min(),ymin))
 #         dy=103.
         dx=(x.max()-x.min())+1.0
         f=imgdy/imgdx
         a=aspect_ratio
-        dh=dy*f/(a-f)
-        pict.x=pict.x/imgdx*dx-0.5+x.min()
-        pict.y=pict.y/imgdy*dh+y.min()
+        dh=dy*f/np.abs(a-f)
+        pixel_width=dx/imgdx
+#         pict.x=pict.x/(imgdx-1)*(dx-pixel_width)-0.5+x.min()+pixel_width/2
+        pict.x=pict.x/(imgdx-1)*(dx)-0.5+x.min()
+        pict.y=pict.y/imgdy*dh+min(y.min(),ymin)
 #         pict.x=pict.x/1416.*103.-0.5
 #         pict.y=pict.y/64.*1.
 
